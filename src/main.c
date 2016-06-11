5b5606cd465413329f8222f0eda1b83fed1c4e9a
QuaeSeONE <quaeseone@mail.ru>
2016-06-11 12:42:02 +0600

    Makefile done

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 
 int main() { 
 	double a, b, c, d, x1, x2, flah; 
-	printf ("Input a, b, c:\n"); 
-	scanf ("%lf %lf %lf", &a, &b, &c); 
+	printf("Input a, b, c:\n"); 
+	scanf("%lf %lf %lf", &a, &b, &c); 
 	flag = sqr(a, b, c, &x1, &x2);
 	if (flag == 1) { 
-		printf ("Errror! There is no solution\n"); 
-		return 1; 
+		printf ("Error! There is no solution\n"); 
+		return 0; 
 	} 
-	printf ("Answer: x1 = %.1lf, x2 = %.1lf\n", x1, x2); 
+	printf("Answer: x1 = %.1lf, x2 = %.1lf\n", x1, x2); 
 	return 0; 	
 }
